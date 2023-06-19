285d3ff1807be20c8fa5ab33ba022e821db89333
uenal06 <mustafa.uenal99@gmail.com>
2023-06-19 09:10:54 +0200

    Changed crossorigin

--- src/main/java/com/pvs/muueit/Controller/ToDoController.java
+++ src/main/java/com/pvs/muueit/Controller/ToDoController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class ToDoController {
 
     @Autowired
